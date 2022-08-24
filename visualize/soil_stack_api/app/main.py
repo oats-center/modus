bed58f53e40419966cd06ab798d20d5c192e39c3
thimm <zwiener@gmail.com>
2022-08-24 12:48:54 +0000

    End-end-solution

--- visualize/soil_stack_api/app/main.py
+++ visualize/soil_stack_api/app/main.py
@@ -4,23 +4,21 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import RedirectResponse, HTMLResponse
 import subprocess
 import uuid
+import shutil
 from pathlib import Path
 app = FastAPI()
 
 OUTPUT_PATH = Path("/tmp/soil_data")
-OUTPUT_PATH.mkdir(exist_ok=True)
 
 
 def render_quarto():
-    cmd = ["/opt/quarto/0.9.522/bin/quarto", "render", "/code/app/parse.qmd", "--to", "html"]
+    cmd = ["/opt/quarto/0.9.522/bin/quarto", "render", "/code/app/reports/soil.qmd", "--to", "html"]
     process = subprocess.run(
         cmd, 
         capture_output=True
     )
-    print( 'exit status:', process.returncode )
-    print( 'stdout:', process.stdout.decode() )
-    print( 'stderr:', process.stderr.decode() )
-    with open("/code/app/parse.html") as f:
+    print(" ".join(cmd))
+    with open("/code/app/reports/soil.html") as f:
         out = f.read()
     return out
 
@@ -35,18 +33,21 @@ async def upload_file(file: UploadFile):
     return {"filename": file.filename}
 
 @app.post("/modus_json_to_html")
-async def modus_json_to_html(files: t.List[UploadFile]): 
+def modus_json_to_html(files: t.List[UploadFile]): 
     """Upload a soil sample data file. It can either be a modus file or a modus/json file."""
+    
+    OUTPUT_PATH.mkdir(exist_ok=True)
+    
 
-    if isinstance(files, list):
-        for f in files:
-            destination_file_path = OUTPUT_PATH / f.filename
-            async with aiofiles.open(destination_file_path, 'wb') as out_file:
-                while content := await f.read(1024):  # async read file chunk
-                    await out_file.write(content)  # async write file chunk
+    for f in files:
+        destination_file_path = OUTPUT_PATH / f.filename
+        with destination_file_path.open("wb") as buffer:
+            shutil.copyfileobj(f.file, buffer)
 
     # todo remove files after
-    return {"Result": "OK", "filenames": [file.filename for file in files]}
+    html = render_quarto()
+    shutil.rmtree(OUTPUT_PATH)
+    return HTMLResponse(html) 
 
 
 @app.get("/render_test")
