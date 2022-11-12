8d9fc778e496996770ec780e041d02330d2f86e7
Aaron Ault <aultac@gmail.com>
2022-11-12 12:39:50 -0500

    Moved zip.ts into json.ts since they were a circular dependency.  Fixes the build problem with Rollup.

--- convert/src/json.ts
+++ convert/src/json.ts
@@ -2,12 +2,12 @@
 // with suggested output filenames.  Just give each input file a filename whose extension
 // reflects its type, and
 import debug from 'debug';
+import jszip from 'jszip';
 import ModusResult, {
   assert as assertModusResult,
 } from '@oada/types/modus/v1/modus-result.js';
-import { parse as csvParse, supportedFormats } from './csv.js';
+import { parse as csvParse, supportedFormats, SupportedFormats } from './csv.js';
 import { parseModusResult as xmlParseModusResult } from './xml.js';
-import { parse as zipParse } from './zip.js';
 
 const error = debug('@modusjs/convert#tojson:error');
 const warn = debug('@modusjs/convert#tojson:error');
@@ -219,3 +219,55 @@ export function typeFromFilename(filename: string): SupportedFileType | null {
   if (filename.match(/.zip/)) return 'zip';
   return null;
 }
+
+
+// I moved all the Zip file stuff to json.ts because json.ts and zip.ts were a circular dependency, and
+// nothing but json.ts actually imported the zip.ts stuff.
+
+export type ZipFile = {
+  filename: string;
+  arrbuf?: ArrayBuffer;
+  base64?: string;
+  format?: SupportedFormats;
+};
+
+export async function zipParse(file: ZipFile) {
+  let opts = {};
+  const data = file.arrbuf || file.base64;
+  if (file.base64) {
+    opts = { base64: true };
+  }
+  if (!data) {
+    error(
+      'ERROR: Zip input file had neither arrbuf nor base64.  At least one is required.'
+    );
+    throw new Error(
+      'Zip must have either array buffer or base64-encoded string'
+    );
+  }
+
+  const zip = await jszip.loadAsync(data, opts);
+  let all_convert_inputs = [];
+  for (const zf of Object.values(zip.files)) {
+    if (zf.dir) continue;
+    const type = typeFromFilename(zf.name);
+    // Strip all path info from the filename:
+    const filename = zf.name.replace(/^(.*[\/\\])*/g, '');
+    trace('Found file', filename, 'of type', type, 'in zip');
+    let convert_input: InputFile = { filename, format: file.format };
+    switch (type) {
+      case 'zip':
+      case 'xlsx':
+        convert_input.arrbuf = await zf.async('arraybuffer');
+        break;
+      case 'xml':
+      case 'csv':
+      case 'json':
+        convert_input.str = await zf.async('string');
+        break;
+    }
+    all_convert_inputs.push(convert_input);
+  }
+  return toJson(all_convert_inputs);
+}
+
