dd0ebeaaa23b46d7f638ced76aa75c3fa7ba8178
Aaron Ault <aultac@gmail.com>
2022-12-02 08:59:17 -0500

    Update

---
+++ app/src/state/initialize.ts
@@ -0,0 +1,3 @@
+export function initialize() {
+  // Put anything here that needs to run when the app starts up
+}
--- convert/src/index.ts
+++ convert/src/index.ts
@@ -3,7 +3,7 @@ export * as csv from './csv.js';
 export * as json from './json.js';
 export * as units from './units.js';
 // Promote type to top-level export
-export { ModusResult } from './json.js';
+export { ModusResult, InputFile } from './json.js';
 
 // exported from the platform-specific code node/ and browser/
 //export * as html from './html.js';
