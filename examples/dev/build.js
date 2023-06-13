b8f38d9baa91a18bec26aea4b6ef44213a511fed
Sam Noel <noel.samuel.a@gmail.com>
2023-06-12 22:25:11 -0400

    bigdemo.zip

--- examples/dev/build.js
+++ examples/dev/build.js
@@ -21,6 +21,8 @@ import fs from 'fs/promises';
     const typedirs = await fs.readdir(`./examples/${labdir}`);
     const lab_path = `./build/${labdir}`;
     let lab_index = '';
+    const labdirstat = await fs.stat('./examples/'+labdir);
+    if (!labdirstat.isDirectory()) continue;
     let lab_all = {};
     for (const typedir of typedirs) {
       const dir = `${labdir}/${typedir}`;
